8d19ee26b3e7c0d43ba158c0a3babca0c5829009
Erik van Leeuwen <erikvl87@pm.me>
2026-02-17 20:57:03 +0100

    Update API examples to use new SDK methods

--- script.example.js
+++ script.example.js
@@ -1,10 +1,10 @@
 const app = await Homey.apps.getApp({ id: 'nl.jwienk.todoist' });
-const users = await app.apiGet('/users');
+const users = await app.get({path: '/users'});
 
 // if you have multiple users pick the right one
 const me = users[0];
 const userId = me.id;
-const user = await app.apiGet(`/users/${userId}`);
+const user = await app.get({path: `/users/${userId}`});
 
 // should be equal
 console.log({
@@ -12,27 +12,27 @@ console.log({
   user
 });
 
-const projects = await app.apiGet(`/users/${userId}/projects`);
+const projects = await app.get({path: `/users/${userId}/projects`});
 const firstProject = projects[0] || {};
-const project = await app.apiGet(`/users/${userId}/projects/${firstProject.id}`);
+const project = await app.get({path: `/users/${userId}/projects/${firstProject.id}`});
 
 console.log({
   firstProject,
   project
 });
 
-const projectTasks = await app.apiGet(`/users/${userId}/projects/${firstProject.id}/tasks`);
+const projectTasks = await app.get({path: `/users/${userId}/projects/${firstProject.id}/tasks`});
 const firstProjectTask = projectTasks[0] || {};
-const projectTask = await app.apiGet(`/users/${userId}/projects/${firstProject.id}/tasks/${firstProjectTask.id}`);
+const projectTask = await app.get({path: `/users/${userId}/projects/${firstProject.id}/tasks/${firstProjectTask.id}`});
 
 console.log({
   firstProjectTask,
   projectTask
 });
 
-const allTasks = await app.apiGet(`/users/${userId}/tasks`);
+const allTasks = await app.get({path: `/users/${userId}/tasks`});
 const allTasksFirstTask = allTasks[0] || {};
-const task = await app.apiGet(`/users/${userId}/tasks/${allTasksFirstTask.id}`);
+const task = await app.get({path: `/users/${userId}/tasks/${allTasksFirstTask.id}`});
 
 console.log({
   allTasksFirstTask,
@@ -40,28 +40,28 @@ console.log({
 })
 
 // https://developer.todoist.com/rest/v2/?shell#create-a-new-task
-const createdTask = await app.apiPost(`/users/${userId}/tasks`, { content: 'My task' });
-const createdTask2 = await app.apiPost(`/users/${userId}/tasks`, { content: 'My task2' });
-const createdTask3 = await app.apiPost(`/users/${userId}/tasks`, { content: 'My task3' });
+const createdTask = await app.post({path: `/users/${userId}/tasks`, body: { content: 'My task' }});
+const createdTask2 = await app.post({path: `/users/${userId}/tasks`, body: { content: 'My task2' }});
+const createdTask3 = await app.post({path: `/users/${userId}/tasks`, body: { content: 'My task3' }});
 // https://developer.todoist.com/rest/v2/?shell#update-a-task
-await app.apiPost(`/users/${userId}/tasks/${createdTask.id}`, { content: 'My updated task' });
+await app.post({path: `/users/${userId}/tasks/${createdTask.id}`, body: { content: 'My updated task' }});
 
 console.log({
   createdTask,
 })
 
 // complete/close task
-await app.apiPost(`/users/${userId}/tasks/${createdTask.id}/close`);
-await app.apiPost(`/users/${userId}/tasks/${createdTask2.id}/close`);
+await app.post({path: `/users/${userId}/tasks/${createdTask.id}/close`});
+await app.post({path: `/users/${userId}/tasks/${createdTask2.id}/close`});
 // reopen task
-await app.apiPost(`/users/${userId}/tasks/${createdTask.id}/reopen`);
+await app.post({path: `/users/${userId}/tasks/${createdTask.id}/reopen`});
 
 // delete task
-await app.apiDelete(`/users/${userId}/tasks/${createdTask3.id}`);
+await app.delete({path: `/users/${userId}/tasks/${createdTask3.id}`});
 
 await tag('Task ID', createdTask.id);
 global.set("taskId", createdTask.id);
 
 // from other script
 const taskId = global.get('taskId');
-await app.apiPost(`/users/${userId}/tasks/${taskId}/close`);
+await app.post({path: `/users/${userId}/tasks/${taskId}/close`});
